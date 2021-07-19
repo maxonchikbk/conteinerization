d628ea7ffa1caa6150aa9984de433e77971363b8
Max <maxonchik@bk.ru>
2021-07-19 22:18:03 +0300

    elastic

--- homework/Monitoring/uninstall.bat
+++ homework/Monitoring/uninstall.bat
@@ -1,4 +1,5 @@
 docker-compose -f ./nginx/docker-compose.yml down
 docker-compose -f ./prom/docker-compose.yml down    
+docker-compose -f ./elastic/docker-compose.yml down
 docker-compose -f ./zabbix/docker-compose.yml down
 docker-compose --env-file ./sentry/.env -f ./sentry/docker-compose.yml down
